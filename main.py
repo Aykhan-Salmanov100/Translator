aea2be6bf900a8b4221058643481d463c54ecf79
Aykhan-Salmanov100 <165781530+Aykhan-Salmanov100@users.noreply.github.com>
2024-04-02 11:26:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-class Main():
-    def __init__(self):
-        pass
-
-    def start_app():
-        pass
+def start_app():
+    pass
 
 
 
 if __name__ == "__main__":
-    MainClass = Main()
-    MainClass.start_app()
+    start_app()
