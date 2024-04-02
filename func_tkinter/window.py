3e9fb6631a51490b974a133737f72a449d5b8598
Aykhan-Salmanov100 <165781530+Aykhan-Salmanov100@users.noreply.github.com>
2024-04-02 12:06:05 +0100

    v 0.0.2
    1. "Added 'func_tkinter' folder"
    2. "Moved import statement for Translator into 'start.py'"
    3. "Updated structure of 'start.py' file"

---
+++ func_tkinter/window.py
@@ -0,0 +1,6 @@
+import tkinter
+
+def window_create():
+    my_win = tkinter.tkinter()
+    my_win.title("Translator")
+    my_win.geometry("350x25")
---
+++ start.py
@@ -0,0 +1,8 @@
+from googletrans import Translator
+from func_tkinter.window import window_create
+
+def start_app():
+    window_create()
+
+if __name__ == "__start__":
+    start_app()
