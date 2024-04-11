1b0c2277eb3c4bea14a7115802f1867577609f5c
Aykhan-Salmanov100 <165781530+Aykhan-Salmanov100@users.noreply.github.com>
2024-04-11 22:57:08 +0300

    Added translation functionality using Google Translate API

--- func_translate/translate.py
+++ func_translate/translate.py
@@ -1,4 +1,6 @@
 from googletrans import Translator
 
-def translate():
-    pass
+def translate_text(text, target_language):
+    translator = Translator()
+    translated_text = translator.translate(text, dest=target_language)
+    return translated_text.text
