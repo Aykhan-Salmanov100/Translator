edded35f1a089d7accf2d88aa7c0f9ec22286928
Aykhan-Salmanov100 <165781530+Aykhan-Salmanov100@users.noreply.github.com>
2024-04-12 17:39:56 +0300

    Modularize GUI setup and functionality

---
+++ func_translate/translation_function.py
@@ -0,0 +1,6 @@
+from func_translate.translate import translate_text
+
+# Function to translate text
+def translate_text(text, target_language):
+    translated_text = translate_text(text, target_language)
+    return translated_text
